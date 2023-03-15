6a18f6bfe1f5a8a79978e95433a975bb6719666f
kavinits <45655428+kavinits@users.noreply.github.com>
2023-03-15 21:16:38 +0530

    Add files via upload

---
+++ Formula +3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main()
+{
+	int x=2,y=4,xy;
+	//x3+3xy+y3
+	xy=(x*x*x)+3*(x*y)+(y*y*y);
+	printf("xy=%d",xy);
+}
---
+++ Formula -.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main()
+{
+	int x=6,y=8,xy;
+	//x2-2xy-y2
+	xy=(x*x)-2*(x*y)-(y*y);
+	printf("xy=%d",xy);
+}
